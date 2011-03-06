2609202bb866cc0ee5df127c6332ffc9ee8046ca
Anand Kumria <akumria@acm.org>
2011-03-06 05:18:12 +0000

    Move this out of the definition, otherwise we it is defined twice.
     Which you should not do in puppet.

--- manifests/ppa.pp
+++ manifests/ppa.pp
@@ -1,14 +1,14 @@
 # ppa.pp
 
+package { "python-software-properties":
+	ensure => installed
+}
+
 define apt::ppa(
 	
 ) {
 	include apt
 	
-	package { "python-software-properties":
-		ensure => installed
-	}
-
 	exec { "/usr/bin/add-apt-repository ${name}":
 		require => Package["python-software-properties"],
 		# TODO: unless => 'check'
