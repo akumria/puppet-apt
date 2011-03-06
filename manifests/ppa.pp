05aebf7e6282a60118c1e4b6bf4b1a082804885e
Anand Kumria <akumria@acm.org>
2011-03-06 02:11:55 +0000

    Ensure that the python-software-properties package is installed.

--- manifests/ppa.pp
+++ manifests/ppa.pp
@@ -5,6 +5,10 @@ define apt::ppa(
 ) {
 	include apt
 	
+	package { "python-software-properties":
+		ensure => installed
+	}
+
 	exec { "/usr/bin/add-apt-repository ${name}":
 		require => Package["python-software-properties"],
 		# TODO: unless => 'check'
